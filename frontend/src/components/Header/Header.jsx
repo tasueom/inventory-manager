75c563d6e6c909ab5b2c864f775af173012f35d0
tasueom <tasueom@naver.com>
2025-12-18 12:00:51 +0900

    f 헤더

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>재고 관리 시스템</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
