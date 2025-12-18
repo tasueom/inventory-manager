d48da854f7a84b784717eef987470668bcc3c4f5
tasueom <tasueom@naver.com>
2025-12-18 12:07:39 +0900

    f 푸터

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <p>Inventory Manager | Built with React + Spring Boot</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
