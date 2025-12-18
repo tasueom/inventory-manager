27ccb2c02db9a00441deeb950b7171982e2941f5
tasueom <tasueom@naver.com>
2025-12-18 15:25:42 +0900

    f 등록 페이지 제거

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -11,7 +11,6 @@ function Header() {
         <ul>
           <li><Link to="/">홈</Link></li>
           <li><Link to="/view">상품 목록</Link></li>
-          <li><Link to="/create">상품 등록</Link></li>
         </ul>
       </nav>
     </>
