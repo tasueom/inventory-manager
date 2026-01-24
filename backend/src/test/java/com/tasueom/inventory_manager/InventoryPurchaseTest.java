3bf1b0b2d0cb98bc9c4534d012667d5aa8054371
tasueom <tasueom@naver.com>
2026-01-24 17:49:51 +0900

    b H2 데이터베이스 의존성 추가 및 테스트 설정 파일 생성

--- backend/src/test/java/com/tasueom/inventory_manager/InventoryPurchaseTest.java
+++ backend/src/test/java/com/tasueom/inventory_manager/InventoryPurchaseTest.java
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.OptimisticLockingFailureException;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class InventoryPurchaseTest {
